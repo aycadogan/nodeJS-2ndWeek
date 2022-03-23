540c28907aaa2a4c7b62ee0230bd031c3e06a826
aycadogan89 <doganayca89@gmail.com>
2022-03-23 02:02:59 -0700

    Added product detail view and its logic

--- nodeJS200/routes/shop.route.js
+++ nodeJS200/routes/shop.route.js
@@ -3,9 +3,17 @@ const router = express.Router()
 
 const shopController = require('../controllers/shop.controller')
 
+//regular user
 router.get('/', shopController.getProducts)
+router.get('/products/:productId', shopController.getProductById)
+
+//-------middleware to stop regular users from accessing below routes------------
+
+//admins
 router.get('/add-product', shopController.getAddProduct)
 router.post('/add-product', shopController.postAddProduct)
 router.get('/edit-product/:productId', shopController.getEditProduct)
 router.post('/edit-product',shopController.postEditProduct)
+router.post('/delete-product', shopController.postDeleteProduct)
+
 module.exports = router
