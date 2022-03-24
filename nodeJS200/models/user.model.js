c7ed7cf903ba2389f44f22986410d1c2c8a0e5aa
aycadogan89 <doganayca89@gmail.com>
2022-03-24 02:36:37 -0700

    mongoose product and user schema

--- nodeJS200/models/user.model.js
+++ nodeJS200/models/user.model.js
@@ -1,35 +1,100 @@
-const mongodb = require('mongodb')
-const getDB = require('../util/db-mongo').getDB
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-module.exports = class User{
-
-    constructor(username, email){
-        this.username = username
-        this.email = email
+const userSchema = new Schema({
+    username: {
+        type:String,
+        required:true
+    },
+    email: {
+        type:String,
+        required:true
+    },
+    cart: {
+        items: [{
+            productId:{
+                type: Schema.Types.ObjectId,
+                required: true
+            },
+            quantity:{
+                type: Number,
+                required: true
+            }
+        }]
     }
+})
 
-    save(){
-        const db = getDB()
-        return db.collection('users').insertOne(this)
-    }
+//custom methods of mongoose
 
-    edit(id){
-        const db = getDB()
-        return db.collection('users').updateOne({_id: new mongodb.ObjectId(id)}, {$set:this})
-    }
+userSchema.methods.addToCart = function(product){
+   const cartProductIndex = this.cart.items.findIndex(item => item.productId.toString() === product._id.toString() )
 
-    static fetchAll(){
-        const db = getDB()
-        return db.collection('users').find().toArray()
-    }
+   let newQuantity = 1
 
-    static findById(id){
-        const db = getDB()
-        return db.collection('users').findOne({_id: new mongodb.ObjectId(id)})
-    }
+   const updatedCartItems = [...this.cart.items]
 
-    static deleteById(){
-        const db = getDB()
-        return db.collection('users').deleteOne({_id: new mongodb.ObjectId(id)})
-    }
+   if(cartProductIndex >=0){
+       newQuantity = this.cart.items[cartProductIndex].quantity + 1
+       updatedCartItems[cartProductIndex].quantity = newQuantity
+   }else{
+       updatedCartItems.push({
+           productId:product._id,
+           quantity: newQuantity
+       })
+   }
+
+   const updatedCart = {
+       items: updatedCartItems
+   }
+
+   this.cart = updatedCartItems
+   return this.save()
 }
+
+userSchema.methods.removeFromCart = function(productId){
+
+    const updatedCartItems = this.cart.items.filter(item => item.productId.toString() !== productId.toString())
+    this.cart.item = updatedCartItems
+    return this.save()
+}
+
+module.exports = mongoose.model('User', userSchema)
+
+
+
+//--------------- MONGODB------------------------
+// const mongodb = require('mongodb')
+// const getDB = require('../util/db-mongo').getDB
+
+// module.exports = class User{
+
+//     constructor(username, email){
+//         this.username = username
+//         this.email = email
+//     }
+
+//     save(){
+//         const db = getDB()
+//         return db.collection('users').insertOne(this)
+//     }
+
+//     edit(id){
+//         const db = getDB()
+//         return db.collection('users').updateOne({_id: new mongodb.ObjectId(id)}, {$set:this})
+//     }
+
+//     static fetchAll(){
+//         const db = getDB()
+//         return db.collection('users').find().toArray()
+//     }
+
+//     static findById(id){
+//         const db = getDB()
+//         return db.collection('users').findOne({_id: new mongodb.ObjectId(id)})
+//     }
+
+//     static deleteById(){
+//         const db = getDB()
+//         return db.collection('users').deleteOne({_id: new mongodb.ObjectId(id)})
+//     }
+// }
